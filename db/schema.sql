88b9e96c6b3c154a8d325c9abf81da9ba65a5891
Nikolas Mazur <kolemazur@gmail.com>
2022-05-13 08:21:08 -0600

    Reformat issues with roles table

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,16 @@
-DROP DATABASE IF EXISTS company_database;
-CREATE DATABASE company_database;
+DROP DATABASE IF EXISTS company_database; --Replace 'company_database' with company name.
+CREATE DATABASE company_database; --Replace 'company_database' with company name.
 
-USE company_database;
+USE company_database; --Replace 'company_database' with company name.
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     job_title VARCHAR(60) NOT NULL,
-    role_id INT NOT NULL,
     department_id INT NOT NULL,
     salary DECIMAL NOT NULL
     FOREIGN KEY (department_id)
@@ -26,7 +25,7 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES roles(id)
     ON DELETE CASCADE
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
