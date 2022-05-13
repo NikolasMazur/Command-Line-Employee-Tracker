505452c3b0091974625127f0b6354fe112376d2e
Nikolas Mazur <kolemazur@gmail.com>
2022-05-13 07:52:10 -0600

    Add employee table

--- db/schema.sql
+++ db/schema.sql
@@ -18,3 +18,17 @@ CREATE TABLE role (
     REFERENCES department(id)
     ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(60) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE CASCADE
+);
