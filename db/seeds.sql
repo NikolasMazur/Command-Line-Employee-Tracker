0e1067cfbd0337f708873998e5e658a26a8d05e4
Nikolas Mazur <kolemazur@gmail.com>
2022-05-13 09:26:25 -0600

    Add roles seed

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO roles (job_title, department_id, salary)
+VALUES ("OTR/L", 1, 125830) --Replace "OTR/L" with desired role, 1 with desired ID, 125830 with desired salary.
+       ("PT", 1, 76181) --Replace "PT" with desired role, 1 with desired ID, 76181 with desired salary.
+       ("DPT", 1,83121 ) --Replace "DPT" with desired role, 1 with desired ID, 83121 with desired salary.
+       ("IT", 2, 76379) --Replace "IT" with desired role, 1 with desired ID, 76379 with desired salary.
+       ("Office Manager", 3, 56723) --Replace "Office Manager" with desired role, 1 with desired ID, 56723 with desired salary.
+
