328d9f8a1105e45f1c1d146aabc5a827ddf14c12
Nikolas Mazur <kolemazur@gmail.com>
2022-05-12 23:17:00 -0600

    Add role table

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,14 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(60) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(60) NOT NULL,
+    role_id INT NOT NULL,
+    department_id INT NOT NULL,
+    salary DECIMAL NOT NULL
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
+);
