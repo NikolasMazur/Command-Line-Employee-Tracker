1c09bdc52a6f054c79b21ca19163556f7af4014b
Nikolas <kolemazur@gmail.com>
2022-05-16 07:11:02 -0600

    Fix schema.sql and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,28 +5,27 @@ USE company_database;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(60) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    job_title VARCHAR(60) NOT NULL,
+    job_title VARCHAR(30),
     department INT,
-    salary DECIMAL NOT NULL,
-    CONSTRAINT fk_department
-    FOREIGN KEY (department)
-    REFERENCES department(id)
+    salary DECIMAL,
+    FOREIGN KEY (department) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(60) NOT NULL,
-    last_name VARCHAR(60) NOT NULL,
-    role_id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES roles(id),
+    CONSTRAINT fk_manager
     FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    REFERENCES employee(id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,20 @@
-INSERT INTO department(id, department_name)
-VALUES (1, "Therapy Staff"),
-       (2, "Information Technology"),
-       (3, "Administration");
+use company_database;
 
-INSERT INTO roles(id, job_title, department, salary)
-VALUES (1, "OTR/L", 1, 125830),
-       (2, "PT", 1, 76181),
-       (3, "DPT", 1,83121 ),
-       (4, "IT", 2, 76379),
-       (5, "Office Manager", 3, 56723);
+INSERT INTO department(department_name)
+VALUES ("Therapy Staff"),
+       ("Information Technology"),
+       ("Administration");
 
-INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
-VALUES (1, "John", "Doe", 1, 1),
-       (2, "Jane", "Doe", 2, 3),
-       (3, "Liam", "Smith", 3, 3),
-       (4, "Olivia", "Williams", 4, 5),
-       (5, "Noah", "Baker", 5, 5);
+INSERT INTO roles(job_title, department, salary)
+VALUES ("OTR/L", 1, 125830),
+       ("PT", 1, 76181),
+       ("DPT", 1,83121 ),
+       ("IT", 2, 76379),
+       ("Office Manager", 3, 56723);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, NULL),
+       ("Jane", "Doe", 2, NULL),
+       ("Liam", "Smith", 3, NULL),
+       ("Olivia", "Williams", 4, NULL),
+       ("Noah", "Baker", 5, NULL);
